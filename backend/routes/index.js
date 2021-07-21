12b423598a0fd8c01d7dbd21e9df3400235e9272
Deniz <sea-97@hotmail.com>
2021-07-21 17:40:06 +0300

    routes düzenledi

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/campains', function(req, res, next) {
+  res.render('campains');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
 module.exports = router;
