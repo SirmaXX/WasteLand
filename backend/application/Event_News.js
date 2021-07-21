debf0300698b05382965a7e26f99c70e2e20c30f
Deniz <sea-97@hotmail.com>
2021-07-21 20:28:56 +0300

    uygulama katmanı eklendi

---
+++ backend/application/Event_News.js
@@ -0,0 +1,18 @@
+class Event {
+  constructor(id,name,hours, date, place, img) {
+    this.id = id;
+    this.name = name;
+    this.hours = hours;
+    this.date = date;
+    this.place = place;
+    this.img = img;
+  }
+}
+
+class News {
+  constructor(id,name) {
+    this.id=id;
+    this.name = name;
+   
+  }
+}
